fe1227c1f7adb48779745197b3bf0383af344cfd
camosaan <csturnbull@live.co.uk>
2016-06-26 06:14:24 +0100

    auto deploy script updates

--- auto_deploy_dsp.sh
+++ auto_deploy_dsp.sh
@@ -5,7 +5,7 @@
 current_dir=$(pwd)
 
 echo ""
-echo "Capturing Required Config for install:"
+echo "## Capturing Required Config for install:"
 echo ""
 read -p "Please enter install base install directory: " installdir
 read -p "Please enter MySQL host address (127.0.0.1): " sqlhost
@@ -20,33 +20,45 @@ read -p "Please enter the IP address of the LOGIN server (127.0.0.1): " loginip
 read -p "Plese enter the IP address of the MAP server (127.0.0.1): " mapip 
 read -p "Plese enter the IP address of the MESSAGE server (shared across all configs): " msgip
 
-echo "Preparing to deploy DSP: Quetz..."
+echo ""
+echo "##Preparing to deploy DSP: Quetz..."
+echo ""
 
-echo "Creating install directory in: '${installdir}' ..."
+echo ""
+echo "##Creating install directory in: '${installdir}' ..."
+echo ""
 mkdir -p ${installdir}
 mkdir -p ${installdir}/tmp
-mkdir -p ${installdir}/dsp && echo "Install dirs created..."
+mkdir -p ${installdir}/dsp && echo "	--Install dirs created..."
 
-echo "Deploying installation and auto-setup files..."
+echo ""
+echo "##Deploying installation and auto-setup files..."
+echo ""
 
 cp ${current_dir}/install_dsp.tz ${installdir}/
 
-tar xf ${installdir}/install_dsp.tz -C ${installdir}/tmp/ && echo "Installation files ready..."
+tar xf ${installdir}/install_dsp.tz -C ${installdir}/tmp/ && echo "	--Installation files ready..."
 
-echo "Creating DB User & Database..."
+echo ""
+echo "##Creating DB User & Database..."
+echo ""
 
-sed -i 's/zoneipreplace/'${zoneip}'/g' ${installdir}/tmp/zone_ip.sql && echo "Zone IP configured..."
-sed -i 's/dbusername'/${dbusername}'/g' ${installdir}/tmp/create_users.sql && echo "DB user configured..."
-sed -i 's/dbuserpass/'${dbuserpass}'/g' ${installdir}/tmp/create_users.sql && echo "DB password configured..."
-sed -i 's/dbname/'${dbname}'/g' ${installdir}/tmp/create_users.sql && echo "DB name configured..."
+sed -i 's/zoneipreplace/'${zoneip}'/g' ${installdir}/tmp/zone_ip.sql && echo "	--Zone IP configured..."
+sed -i 's/dbusername'/${dbusername}'/g' ${installdir}/tmp/create_users.sql && echo "	--DB user configured..."
+sed -i 's/dbuserpass/'${dbuserpass}'/g' ${installdir}/tmp/create_users.sql && echo "	--DB password configured..."
+sed -i 's/dbname/'${dbname}'/g' ${installdir}/tmp/create_users.sql && echo "	--DB name configured..."
 
-mysql -u ${mysqlrootuser} -p${mysqlrootpass} < ${installdir}/tmp/create_users.sql && echo "User & Database created successfully!"
+mysql -u ${mysqlrootuser} -p${mysqlrootpass} < ${installdir}/tmp/create_users.sql && echo "	--User & Database created successfully!"
 
-echo "Grabbing stable DSP branch..."
+echo ""
+echo "##Grabbing stable DSP branch..."
+echo ""
 
 git clone http://github.com/DarkstarProject/darkstar.git/ ${installdir}/dsp/
 
-## Now the actual init of the dabatase:
+echo ""
+echo "## Now the actual init of the dabatase:"
+echo ""
 
 for i in ${installdir}/dsp/sql/*.sql
         do
@@ -56,38 +68,44 @@ for i in ${installdir}/dsp/sql/*.sql
 
 mysql -u ${dbusername} -p${dbuserpass} < ${installdir}/tmp/zone_ip.sql && echo "Zone IP config applied to database..."
 
-echo "Database seed & config complete!"
+echo ""
+echo "	--Database seed & config complete!"
+echo ""
 
-echo "Apply configuration changes to server configs..."
+echo ""
+echo "##Apply configuration changes to server configs..."
+echo ""
 
 sed -i 's/sqlhost'/${sqlhost}'/g' ${installdir}/tmp/map_darkstar.conf
 sed -i 's/msgip'/${msgip}'/g' ${installdir}/tmp/map_darkstar.conf
 sed -i 's/dbusername'/${dbusername}'/g' ${installdir}/tmp/map_darkstar.conf
 sed -i 's/dbuserpass'/${dbuserpass}'/g' ${installdir}/tmp/map_darkstar.conf
-sed -i 's/dbname'/${dbname}'/g' ${installdir}/tmp/map_darkstar.conf && echo "Map server configuration applied..."
+sed -i 's/dbname'/${dbname}'/g' ${installdir}/tmp/map_darkstar.conf && echo "	--Map server configuration applied..."
 
 sed -i 's/loginip'/${loginip}'/g' ${installdir}/tmp/login_darkstar.conf
 sed -i 's/sqlhost'/${sqlhost}'/g' ${installdir}/tmp/login_darkstar.conf
 sed -i 's/msgip'/${msgip}'/g' ${installdir}/tmp/login_darkstar.conf
 sed -i 's/dbusername'/${dbusername}'/g' ${installdir}/tmp/login_darkstar.conf
 sed -i 's/dbuserpass'/${dbuserpass}'/g' ${installdir}/tmp/login_darkstar.conf
-sed -i 's/dbname'/${dbname}'/g' ${installdir}/tmp/login_darkstar.conf && echo "Login server configuration applied..."
+sed -i 's/dbname'/${dbname}'/g' ${installdir}/tmp/login_darkstar.conf && echo "	--Login server configuration applied..."
 
 sed -i 's/sqlhost'/${sqlhost}'/g' ${installdir}/tmp/search_server.conf
 sed -i 's/dbusername'/${dbusername}'/g' ${installdir}/tmp/search_server.conf
 sed -i 's/dbuserpass'/${dbuserpass}'/g' ${installdir}/tmp/search_server.conf
-sed -i 's/dbname'/${dbname}'/g' ${installdir}/tmp/search_server.conf && echo "Search server configuration applied..."
+sed -i 's/dbname'/${dbname}'/g' ${installdir}/tmp/search_server.conf && echo "	--Search server configuration applied..."
 
 cp ${installdir}/tmp/*.conf ${installdir}/dsp/conf/ && echo "...and applied to server."
 
-echo "Time to run the code compile..."
+echo ""
+echo "##Time to run the code compile..."
+echo ""
 
 cd ${installdir}/dsp/
 
-sh autogen.sh && echo "Autogen complete..."
+sh autogen.sh && echo "	--Autogen complete..."
 
-./configure --enable-debug=gdb && echo "Configure complete..."
+./configure --enable-debug=gdb > ${installdir}/dsp/configure_install.log && echo "Configure complete...see configure_install.log for details... "
 
-make && echo "Make complete!"
+make > ${installdir}/dsp/make_install.log && echo "Make complete! See make_install.log for details..."
 
 
